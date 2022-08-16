de7d17a0b130c32410e88f4412e9b4d7d548875c
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2022-08-16 08:57:03 +0300

    happy end

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,10 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
+    
+   
     closeModalBtn: document.querySelector("[data-modal-close]"),
+    
     modal: document.querySelector("[data-modal]"),
   };
 
@@ -10,6 +13,26 @@
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle("modal__is-hidden");
+  }
+})();
+
+
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open-second]"),
+   
+    closeModalBtn: document.querySelector("[data-modal-close-second]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("modal__is-hidden");
   }
 })();
